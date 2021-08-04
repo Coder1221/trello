40f5c087d469f82bf813dfeeb8c052945e891c0b
Coder1221 <21100328@lums.edu.pk>
2021-08-04 18:56:09 +0500

    Error resolved spell error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
+  # devise_for :users, :controllers => {
+  #   :registrations => "users/registrations",
+  #   :sessions => "users/sessions",
+  #   :passwords => "users/passwords",
+  #   :confirmations => "users/confirmations"
+  # }
+  
+  #   confirmations: 'users/confirmations',
+  #   passwords: 'users/passwords'
+
+  # devise_for :users
+  #resourse full routes 
   resources :home do
     member do
       get :delete
@@ -13,4 +29,11 @@ Rails.application.routes.draw do
       get :delete
     end
   end
+
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+
 end
