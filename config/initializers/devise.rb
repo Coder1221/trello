19aac99250318416af3a818f97cd277898713fee
Abdur Rehman <43990250+Coder1221@users.noreply.github.com>
2021-08-25 22:07:34 +0500

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -27,7 +27,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Trello <No reply board_inviation>.co'
+  config.mailer_sender = 'abdurrehman@systemplus.co'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
