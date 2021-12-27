f08b7b0b24f8ae47b7094478e77c080c56e5ccb7
Coder1221 <21100328@lums.edu.pk>
2021-12-28 01:50:11 +0500

    Code Commit

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  let(:list) { List.new }
-  let(:board) { Board.new }
+  let(:user)  { User.create!(username: "test user" , email: 'test@exmaple.com' , password: 'password') }
+  let(:board) { Board.create!(name: "test board", user_id: user.id) }
+  let(:list)  { List.new }
+  
   describe "#initialize" do
-    it "List witout board does not exists" do
-      expect(subject.save).to eq(false)
+    it "with out borad id should not save" do
+      expect(list.save).to eq(false)
     end
+
+    it "when board exits it should save" do
+      list.board_id = board.id
+      list.name = 'Test_board_list'
+      expect(list.save).to eq(true)
+    end
+
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  let(:todo) { Todo.new }
-  it "does not save with out list_id" do
-    # puts subject
-    # puts todo
-    expect(todo.save).to eq(false)
-    # todo.list_id = 1
-    # r
-    # expect(todo.save).to eq(true)
+  let(:user)  { User.create!(username: "test user" , email: 'test@exmaple.com' , password: 'password') }
+  let(:board) { Board.create!(name: "test board", user_id: user.id) }
+  let(:list)  { List.create!(name: "test list" , user_id: user.id , board_id: board.id ) }
+  let(:todo)  { Todo.new }
+
+  describe "Initialization" do
+    it "does not save with out list_id" do
+      expect(todo.save).to eq(false)
+    end
+
+    it "should save with user id" do
+      todo.list_id = list.id
+      expect(todo.save).to eq(true)
+    end
   end
 end
